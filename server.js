e8b340d585b078b46a3637881831705b4be89e33
onepiecetime <peace_under@hotmail.com>
2017-03-27 20:58:56 +0700

    change server.js to use https.createServer and add sth for certbot

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ var passport = require('./config/passport');
 var updateCj = require('./config/updateCronjob');
 var hotCj = require('./config/hotCronjob');
 var config = require('./config/config');
+var https = require('https');
+var fs = require('fs');
 
+var sslPath = '/etc/letsencrypt/live/www.cueventhub.com/';
+
+var options = {
+     key: fs.readFileSync('/path/to/privkey.pem'),
+     cert: fs.readFileSync('/path/to/fullchain.pem'),
+     ca: fs.readFileSync('/path/to/chain.pem')
+}
 
 var db = mongoose();
 var app = express();
@@ -15,6 +24,11 @@ var updateCronJob = updateCj();
 var hotCronJob = hotCj();
 
 
+var server = https.createServer(app);
 
+<<<<<<< HEAD
+server.listen(config.PORT);
+=======
 app.listen(config.PORT, config.IP);
+>>>>>>> 8614f58aaefeae78b7275eee3a4324873223d24d
 console.log("Server is running at " +config.IP +":"+config.PORT);
