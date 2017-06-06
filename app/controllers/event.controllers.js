28a6a27b8b2541bd631c64c0f6373dcf0967b0a7
Panayu <gpanayu@yahoo.co.th>
2017-06-06 10:36:22 +0700

    noti for deleting event

--- app/controllers/event.controllers.js
+++ app/controllers/event.controllers.js
@@ -269,20 +269,66 @@ var updateDeleteEventToChannel = function(channelId,eventId,response){
 					// return next(err);
 				}
 				else {
-					if(request.user){
-						if(request.user.notification != undefined && request.user.notification != null){
-							info.notification = request.user.notification;
-							response.status(200).json(info);
+					notiDeleteEvent(eventId, function(info){
+						if(info.code == 201){
+							if(request.user){
+								if(request.user.notification != undefined && request.user.notification != null){
+									info.notification = request.user.notification;
+									response.status(200).json({msg:info.msg});
+								}
+								else{
+									response.status(200).json({msg:info.msg});
+								}
+							}
+							else{
+								response.status(200).json({msg:info.msg});
+							}
 						}
 						else{
-							response.status(200).json(info);
+							response.status(500).json({msg:info.msg});
 						}
-					}
-					else{
-						response.status(200).json(info);
-					}
+					});
 				}
 			});
+		}
+	});
+};
+
+var notiDeleteEvent = function(eventId, callback){
+	var errorList = [];
+	Event.findById(eventId, function(err, returnedInfo){
+		if(err){
+			response.status(500).json({msg:"internal error."});
+		}
+		else if(!returnedInfo){
+			response.status(404).json({msg:"event not found."});
+		}
+		else{
+			var noti = {};
+			noti.title = returnedInfo.title+" is deleted by channel's administrator.";
+			noti.photo = returnedInfo.picture;
+			noti.source = returnedInfo.title;
+			for(let i=0;i<returnedInfo.who_join;i++){
+				User.findByIdAndUpdate(returnedInfo.who_join[i],{
+					$addToSet : { notification : noti }
+				},function(err, user){
+					if(err || !user){
+						errorList.push(returnedInfo.who_join[i]);
+					}
+				});
+				if(errorList.length == 0){
+					var info={};
+					info.msg = "done";
+					info.code = 201;
+					callback(info);
+				}
+				else{
+					var info={};
+					info.msg = "error";
+					info.code = 500;
+					callback(info);
+				}
+			}
 		}
 	});
 };
