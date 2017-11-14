46f99ba8f117889a7b85cb38932e2d569141fc6b
burin <burin010n@gmail.com>
2017-11-14 23:55:48 +0700

    add feedback model

---
+++ app/models/feedback.model.js
@@ -0,0 +1,28 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+let Moment = require('moment-timezone');
+
+
+let feedbackSchema = new Schema({
+
+	type :{
+		index: true,
+		type: String,
+		trim: true,
+		default: null,
+		required: true
+	},
+	
+	text : {
+		type: String,
+		trim: true,
+	},
+
+	created_date:{
+		type: 'Moment',
+		default: new Moment()
+	},
+
+});
+
+mongoose.model('Feedback',formSchema);
